70e3c4fd900b23a8433c6ab11f9e2b69688b3d3c
Lisa-Ann B <lisabruney@pitt.edu>
2023-08-09 11:43:49 -0400

    Resolve missing method Change to use ops method - #79

--- src/libs/ontology.py
+++ src/libs/ontology.py
@@ -1,6 +1,9 @@
 from atlas_consortia_commons.ubkg.ubkg_sdk import UbkgSDK
 
 
+def enum_val(val):
+    return val.value
+
 # Custom accessors etc. can be added to the Ontology class
 class Ontology(UbkgSDK):
     pass
